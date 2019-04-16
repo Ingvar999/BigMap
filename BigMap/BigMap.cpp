53e3b04e4dcea9304fb01f51b797a0e0233a95fd
Ihar Shymanski <shymanski.ihar@gmail.com>
2019-04-16 15:00:07 +0300

    Init

---
+++ BigMap/BigMap.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "BigMap.h"
+
+template<typename TKey, typename TValue, typename TSyncUnit>
+BigMap<TKey, TValue, TSyncUnit>::BigMap()
+{
+}
+
+template<typename TKey, typename TValue, typename TSyncUnit>
+BigMap<TKey, TValue, TSyncUnit>::~BigMap()
+{
+}
---
+++ BigMap/BigMap.h
@@ -0,0 +1,10 @@
+#pragma once
+
+template<typename TKey, typename TValue, typename TSyncUnit>
+class BigMap
+{
+public:
+	BigMap();
+	~BigMap();
+};
+
